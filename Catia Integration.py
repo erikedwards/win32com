72cda45599b56ee33828d65538b728fdace5ddfe
Erik Edwards <erik.p.edwards@gmail.com>
2021-09-06 00:30:38 -0700

    Added "Catia Integration.py". Works with test catProduct.

--- Catia Integration.py
+++ Catia Integration.py
@@ -5,4 +5,5 @@ a = win32com.client.Dispatch('catia.application')
 doc = a.ActiveDocument.Product
 
 for i in range(doc.Products.Count):
-    print('Part Number: ' + doc.Products.Item(i+1).PartNumber)
+    doc.Products.Item(i+1).PartNumber = "pyPart" + str(i)
+    # print('Part Number: ' + doc.Products.Item(i+1).PartNumber)

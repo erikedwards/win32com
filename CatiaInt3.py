076c23b90f04730d86ea0d022f12b0ac2c16fc73
Erik Edwards <erik.p.edwards@gmail.com>
2021-09-06 23:10:03 -0700

    CatiaInt3 is ready to try fourier vis

--- CatiaInt3.py
+++ CatiaInt3.py
@@ -1,36 +1,106 @@
 import win32com.client
+import math
+
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 500
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# init visualization values
+circX = int(SCREEN_WIDTH / 6)
+circY = int(SCREEN_HEIGHT / 2)
+circScale = 150
+wave = []
+# waveX = int(SCREEN_WIDTH * (2/4))
+waveX = circX
+# waveY = int(SCREEN_HEIGHT / 2)
+time = 0.00
+timeStep = 0.005
+
+# init Catia
 catapp = win32com.client.Dispatch("CATIA.Application")
 rootProd = catapp.ActiveDocument.Product
 
-'''
-for i in range(rootProd.Products.Count):
-    rootProd.Products.Item(i+1).PartNumber = "pyPart_numbero_" + str(10+i)
-    # rootProd.Products.Item(i+1). = ""
-'''
-
-pyPart = catapp.Documents.Item(4).Part
+# grab reference to a catPart (shown here as 4th object, 3rd part) and add a new GeoSet
+pyPart = catapp.Documents.Item(2).Part
 hb1 = pyPart.HybridBodies.Add()
 pyPart.Update()
 
+# create a hybrid shape factory
 hsf = pyPart.HybridShapeFactory
+
+# max "N" to be evaluated. Not the same as number of terms to be evaluated
+# maxN = 5, terms = 1, 3, and 5. Num terms = 3
+# maxN = 11, terms = 1, 3, 5, 7, 9, 11. Num terms = 6
+# num terms = (1/2)*(maxN + 1)
+maxN = 1
+
+# set frame rate
+FR = 60
+
+# Main Loop
+run = True
+
+while run:
+
+    # Game logic
+    time += timeStep
+
+    # --- draw wave CS
+    rad = int(circScale * (4 / (1 * math.pi)))
+    # pygame.draw.line(screen, BLACK, [waveX, circY - rad], [waveX, circY + rad])
+    # pygame.draw.line(screen, BLACK, [waveX - 50, circY], [SCREEN_WIDTH, circY])
+    #TODO --- add lines to Catia geoset "geom_drawAxis"
+
+    x = circX
+    y = circY
+    for n in range(1, maxN +1, 2):
+        prevX = x
+        prevY = y
+        rad = int(circScale * ( 4 / (n * math.pi)))
+        x += int(rad * math.cos(n * time))
+        y += int(-1 * rad * math.sin(n * time))
+        # --- draw circle
+        # pygame.draw.circle(screen, BLACK, [prevX, prevY], rad, 1)
+        # pygame.draw.line(screen, RED, [prevX, prevY], [x, y])
+        #TODO --- add circle and line to Catia geoset "geom_circularMotion"
+
+    # Add y value of smallest circle to wave
+    wave.insert(0, y)
+    if len(wave) > SCREEN_WIDTH:
+        wave.pop(-1)
+
+    # --- draw wave
+    for i in range(0, len(wave)):
+        pX = int(waveX + i)
+        pY = int(wave[i])
+        # pygame.draw.circle(screen, GREEN, [pX, pY], 1)
+        #TODO --- add point to Catia geoset "geom_linearMotion-Sin(theta)"
+        hb1.AppendHybridShape(hsf.AddNewPointCoord(pX, pY, 0))
+
+    pyPart.Update()
+
+    # --- draw connecting level line
+    # pygame.draw.line(screen, GREY, [x, y], [waveX, y])
+
+
+    # --- Update the screen
+    # pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    # clock.tick(FR)
+
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+
+
 for i in range(10):
     point = hsf.AddNewPointCoord(i*10, 0.0, 0.0)
     hb1.AppendHybridShape(point)
+    # pyPart.InWorkObject = point
+    # pyPart.Update()
 pyPart.Update()
 
 
-'''
-documents1 = catapp.Documents
-partDocument1 = documents1.Item(4)
-part1 = partDocument1.Part
-
-hybridBodies1 = part1.HybridBodies
-hybridBody1 = hybridBodies1.Add()
-part1.Update()
-
-hsFactory1 = part1.HybridShapeFactory
-hsPointCoord1 = hsFactory1.AddNewPointCoord(254.0, 254.0, 254.0)
-hybridBody1.AppendHybridShape(hsPointCoord1)
-part1.InWorkObject = hsPointCoord1
-part1.Update()
-'''

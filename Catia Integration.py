371e2ea0de87982a96a669c769353627cc2e792c
Erik Edwards <erik.p.edwards@gmail.com>
2021-09-06 21:49:44 -0700

    Catia Integration.py adds a point to a geoSet CatiaInt2.py walks the product structure tree and returns insatnce name Haven't tried pyCatia yet. makepy didn't help with auto-complete for Catia COM interface

--- Catia Integration.py
+++ Catia Integration.py
@@ -1,10 +1,18 @@
 import win32com.client
+catapp = win32com.client.Dispatch("CATIA.Application")
+rootProd = catapp.ActiveDocument.Product
 
-a = win32com.client.Dispatch('catia.application')
-# a = win32com.client.gencache.EnsureDispatch('catia.application')
+for i in range(rootProd.Products.Count):
+    rootProd.Products.Item(i+1).PartNumber = "pyPart_numbero_" + str(10+i)
+    # rootProd.Products.Item(i+1). = ""
 
-doc = a.ActiveDocument.Product
-
-for i in range(doc.Products.Count):
-    doc.Products.Item(i+1).PartNumber = "pyPart" + str(i)
-    # print('Part Number: ' + doc.Products.Item(i+1).PartNumber)
+documents1 = catapp.Documents
+partDocument1 = documents1.Item("pyPart_numbero_10.CATPart")
+part1 = partDocument1.Part
+hsFactory1 = part1.HybridShapeFactory
+hsPointCoord1 = hsFactory1.AddNewPointCoord(254.0, 254.0, 254.0)
+hybridBodies1 = part1.HybridBodies
+hybridBody1 = hybridBodies1.Item("Geometry")
+hybridBody1.AppendHybridShape(hsPointCoord1)
+part1.InWorkObject = hsPointCoord1
+part1.Update()
---
+++ CatiaInt2.py
@@ -0,0 +1,13 @@
+import win32com.client
+catapp = win32com.client.Dispatch("CATIA.Application")
+doc=catapp.ActiveDocument.Product
+
+product_count = doc.Products.Count
+print("This example lists all Parts and Subproducts of the first level from a CATProduct.")
+for products in range(product_count):
+    products = products + 1
+    print(doc.Products.Item(products).Name + ":")
+    part_count = doc.Products.Item(products).Products.Count
+    for parts in range(part_count):
+        parts = parts +1
+        print ("   " + doc.Products.Item(products).Products.Item(parts).Name)
